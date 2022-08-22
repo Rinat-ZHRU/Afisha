a4f2627abbca8d7528120471904eb2885e724ad8
Rinat_ZHRU <rinat_zhru@rambler.ru>
2022-08-22 17:29:00 +0600

    fix

--- afisha_progect/urls.py
+++ afisha_progect/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('afisha/', include('afisha_app.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
