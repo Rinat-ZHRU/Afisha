d2568e66086b34a1b47bfdf9e2868991abd71424
Rinat_ZHRU <rinat_zhru@rambler.ru>
2022-08-22 16:16:12 +0600

    Deploy

--- afisha_progect/settings.py
+++ afisha_progect/settings.py
@@ -21,10 +21,12 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')    # спрятали секретный ключ (.env)
+# SECRET_KEY = env('SECRET_KEY')    # спрятали секретный ключ (.env)
+SECRET_KEY = 'SECRET_KEY'    # спрятали секретный ключ (.env)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')  # также перенесли в .env
+# DEBUG = env('DEBUG')  # также перенесли в .env
+DEBUG = True  # также перенесли в .env
 
 
 ALLOWED_HOSTS = ['*']
