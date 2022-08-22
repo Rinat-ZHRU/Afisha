a2f18ecfe9ea3f8e6375a801b3422f5b98e75589
Rinat_ZHRU <rinat_zhru@rambler.ru>
2022-08-22 16:04:06 +0600

    Deploy

--- afisha_app/urls.py
+++ afisha_app/urls.py
@@ -3,8 +3,8 @@ from afisha_app.views import CategoryViewSet, ChapterViewSet, GenreViewSet
 
 
 urlpatterns = [
-    path('category/', CategoryViewSet.as_view({'get': 'list'}), name='Список категорий'),
-    path('genre/', GenreViewSet.as_view({'get': 'list'}), name='Список жанров'),
-    path('chapter/', ChapterViewSet.as_view({'get': 'list'}), name='Список разделов'),
-    path('chapter/<int:pk>/', ChapterViewSet.as_view({'get': 'retrieve'}), name='Детализация раздела'),
+    path('/category/', CategoryViewSet.as_view({'get': 'list'}), name='Список категорий'),
+    path('/genre/', GenreViewSet.as_view({'get': 'list'}), name='Список жанров'),
+    path('/chapter/', ChapterViewSet.as_view({'get': 'list'}), name='Список разделов'),
+    path('/chapter/<int:pk>/', ChapterViewSet.as_view({'get': 'retrieve'}), name='Детализация раздела'),
 ]
--- afisha_progect/urls.py
+++ afisha_progect/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('afisha/', include('afisha_app.urls')),
+    path('/admin/', admin.site.urls),
+    path('/afisha/', include('afisha_app.urls')),
 ]
